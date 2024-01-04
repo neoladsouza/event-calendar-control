100c28c08b1b561c855759406a56f991ff5e0643
Neola Dsouza <neola.laurend@gmail.com>
2024-01-04 16:58:05 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,13 @@ import React, {useState} from "react";
 import './App.css';
 import { v4 as uuidv4 } from 'uuid';
 
-const possibleEventTypes = ['Service', 'Appointment', 'Course', 'Workshop', 'Retreat', 'Program'];
+const possibleEventTypes = ['None', 'Service', 'Appointment', 'Course', 'Workshop', 'Retreat', 'Program'];
 
 export default function FormApp() {
   const [eventName, setEventName] = useState('');
+  const [eventDate, setEventDate] = useState('');
+  const [eventStartTime, setStartTime] = useState('');
+  const [eventEndTime, setEndTime] = useState('');
   const [eventType, setEventType] = useState(possibleEventTypes[0]);
   const [eventDesc, setEventDesc] = useState('');
   const [allEvents, setAllEvents] = useState([]);
@@ -25,6 +28,9 @@ export default function FormApp() {
     // every time form is submitted -> create a new object with event name, type, and description properties
     const newEvent = { 
       name: eventName,
+      date: eventDate,
+      startTime: eventStartTime,
+      endTime: eventEndTime,
       type: eventType,
       description: eventDesc,
       id: eventName + uuidv4() // generate unique id for each event
@@ -35,6 +41,15 @@ export default function FormApp() {
   }
   function handleNameChange(e) {
     setEventName(e.target.value);
+  }
+  function handleDateChange(e) {
+    setEventDate(e.target.value);
+  }
+  function handleStartTimeChange(e) {
+    setStartTime(e.target.value);
+  }
+  function handleEndTimeChange(e) {
+    setEndTime(e.target.value);
   }
   function handleTypeChange(e) {
     setEventType(e.target.value);
@@ -53,9 +68,12 @@ export default function FormApp() {
             (<p>No events created</p>) :
             (events.map(event => (
               <ul key={event.id}>
-                <li>Your Event Name: {event.name}</li>
-                <li>Your Event Type: {event.type}</li>
-                <li>Your Event Description: {event.description}</li>
+                <li>Name: {event.name}</li>
+                <li>Date: {event.date}</li>
+                <li>Start Time: {event.startTime}</li>
+                <li>End Time: {event.endTime}</li>
+                <li>Type: {event.type}</li>
+                <li>Description: {event.description}</li>
               </ul>
               ))
             )
@@ -69,7 +87,7 @@ export default function FormApp() {
       <>
         {
           categories.map((value, index) => (
-            <option key={index} value={value.toLowerCase()}>{value}</option>
+            <option key={index} value={value} required>{value}</option>
           ))
         }
       </>
@@ -77,28 +95,45 @@ export default function FormApp() {
   }
 
   // method="post" onSubmit={handleSubmit}
-  // idea ->  big switch statement for handling all input changes?
-  // handle input validation in "handleSubmit"
+  // handle deleting events, accessing events to edit
+  // handle proper input validation in handleSubmit() (call another function for it within handleSubmit)
+  // parse date and time strings to have repeated events and/or visible durations (prime react library?)
+    // parsing dates: splice date string to get each part -> create Date object + compare start date with end date
   return (
     <>
       <form onSubmit={handleSubmit}>
         <ul>
           <li>
             <label>
-              Event Name: <input type="text" name="eventName" value={eventName} onChange={handleNameChange}/>
+              Event Name: <input type="text" name="eventName" value={eventName} onChange={handleNameChange} required/>
+            </label>
+          </li>
+          <li>
+            <label>
+              Event Date: <input type="date" name="eventDate" value={eventDate} onChange={handleDateChange} required/>
+            </label>
+          </li>
+          <li>
+            <label>
+              Event Start Time: <input type="time" name="eventStartTime" min="00:00" max="24:00" value={eventStartTime} onChange={handleStartTimeChange} required/>
+            </label>
+          </li>
+          <li>
+            <label>
+              Event End Time: <input type="time" name="eventEndTime" value={eventEndTime} onChange={handleEndTimeChange} required/>
             </label>
           </li>
           <li>
             <label>
               Event Type:
-              <select name="selectedEventType" value={eventType} onChange={handleTypeChange}>
+              <select name="selectedEventType" value={eventType} onChange={handleTypeChange} required>
                 <Options categories={possibleEventTypes}/>
               </select>
             </label>
           </li>
           <li>
             <label>
-              Event Description: <textarea name="eventDesc" value={eventDesc} onChange={handleDescChange}/>
+              Event Description: <textarea name="eventDesc" value={eventDesc} onChange={handleDescChange} required/>
             </label>
           </li>
         </ul>
