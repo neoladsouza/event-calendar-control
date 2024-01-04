76355403439272281c87ef271ddb93f10bf1d2e1
Neola Dsouza <neola.laurend@gmail.com>
2024-01-03 20:11:15 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,46 @@
-import React from "react";
+import {useState} from "react";
 import './App.css';
 
 export default function FormApp() {
+  const [eventName, setEventName] = useState('');
+  const [eventType, setEventType] = useState('');
+
   function handleSubmit(e) {
     e.preventDefault(); // prevent webpage reloading
 
     const form = e.target; 
     const formData = new FormData(form);
 
-    fetch('/api', {method : form.method, body : formData});
+    // fetch('/api', {method : form.method, body : formData});
+    console.log(new URLSearchParams(formData).toString());
   }
 
   return (
     <>
       <form method="post" onSubmit={handleSubmit}>
         <label>
-          Event Name: <input name="eventName"/>
+          Event Name: <input name="eventName" value={eventName} onChange={e => setEventName(e.target.value)}/>
         </label>
-        <hr/>
-        <p>
+        <br/>
+        <label>
           Event Type:
-          <label>
-            <input type="radio" name="eventType" value="course"/> Course
-          </label>
-          <label>
-            <input type="radio" name="eventType" value="workshop"/> Workshop
-          </label>
-          <label>
-            <input type="radio" name="eventType" value="retreat"/> Retreat
-          </label>
-        </p>
-        <hr/>
+          <select name="selectedEventType" value={eventType} onChange={e => setEventType(e.target.value)}>
+            <option value="course">Course</option>
+            <option value="workshop">Workshop</option>
+            <option value="retreat">Retreat</option>
+          </select>
+        </label>
+        <br/>
         <label>
           Event Description: <textarea name="eventDesc"/>
         </label>
-        <hr/>
+        <br/>
         <button type="reset">Reset form</button>
         <button type="submit">Submit form</button>
+        <hr/>
       </form>
+      {eventName !== '' && <p>Entered Event Name: {eventName}</p>}
+      {eventType !== '' && <p>Entered Event Type: {eventType}</p>}
     </>
   );
 }
