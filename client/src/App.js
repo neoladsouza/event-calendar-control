33e72bad4d163de1069e3fecb2ad9e40b7686af7
Neola Dsouza <neola.laurend@gmail.com>
2024-01-10 23:37:11 -0500

    can upload an excel file, and edit loaded events

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,9 @@ import clsx from 'clsx';
 import { FaEdit, FaRegSave } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
 import { IconContext } from "react-icons";
+import * as XLSX from "xlsx";
 
-// CURRENT GOAL -> handle excel upload
+// CURRENT GOAL -> host this app on AWS S3
 
 class CustomDate extends Date {
   toISOStringWithOffset() {
@@ -59,6 +60,8 @@ export default function FormApp() {
 
   const [selectedDay, setSelectedDay] = useState(null);
   const [eventsForSelectedDay, setEventsForSelectedDay] = useState([]);
+
+  const [excelData, setExcelData] = useState([]);
 
   function handleSubmit(e) {
     e.preventDefault(); // prevent webpage reloading
@@ -152,6 +155,7 @@ export default function FormApp() {
 
     setEventsForSelectedDay(allEvents);
     setSelectedDay(null);
+    console.log(allEvents);
     /*
     // function definition
       try {
@@ -245,37 +249,119 @@ export default function FormApp() {
   return (
     <div className="font-sans my-5 w-full">
       <main className="w-full mx-auto h-auto flex flex-row">
-        <form onSubmit={handleSubmit} className="text-center bg-white shadow-md rounded-xl px-8 pt-6 pb-8 mx-5 w-min h-max border border-blue">
-          <div className="mt-4">
-            <label htmlFor="eventName" className="block text-left text-m font-bold mb-1">Event Name:</label>
-            <input id="eventName" type="text" name="eventName" value={eventName} onChange={handleNameChange} required
-              className="mx-auto shadow appearance-none border rounded w-full py-2 px-3 text-gray-500 leading-tight border-blue focus:border-black" />
+        <section className="mx-5">
+          <form onSubmit={handleSubmit} className="text-center bg-white shadow-md rounded-xl px-8 pt-6 pb-8 w-full h-max border border-blue">
+            <div className="mt-4">
+              <label htmlFor="eventName" className="block text-left text-m font-bold mb-1">Event Name:</label>
+              <input id="eventName" type="text" name="eventName" value={eventName} onChange={handleNameChange} required
+                className="mx-auto shadow appearance-none border rounded w-full py-2 px-3 text-gray-500 leading-tight border-blue focus:border-black" />
+            </div>
+            <div className="mt-4">
+              <label htmlFor="eventStart" className="block text-left text-m font-bold mb-1">Event Start:</label>
+              <input id="eventStart" type="datetime-local" name="eventStart" value={eventStart.toISOStringWithOffset().slice(0, 16)} onChange={handleStartChange} required
+                className="mx-auto shadow appearance-none border rounded w-full py-2 px-3 text-gray-500 leading-tight border-blue focus:border-black hover:cursor-pointer" />
+            </div>
+            <div className="mt-4">
+              <label htmlFor="eventEnd" className="block text-left text-m font-bold mb-1">Event End:</label>
+              <input id="eventEnd" type="datetime-local" name="eventEnd" value={eventEnd.toISOStringWithOffset().slice(0, 16)} onChange={handleEndChange} required
+                className="mx-auto shadow appearance-none border rounded w-full py-2 px-3 text-gray-500 leading-tight border-blue focus:border-black hover:cursor-pointer" />
+            </div>
+            <div className="mt-4">
+              <label htmlFor="selectedEventType" className="block text-left text-m font-bold mb-1">Event Type:</label>
+              <select id="selectedEventType" name="selectedEventType" value={eventType} onChange={handleTypeChange} required
+                className="mx-auto w-full bg-white border border-black px-3 py-2 rounded shadow leading-tight focus:border-black hover:cursor-pointer">
+                <Options categories={allEventTypes} />
+              </select>
+            </div>
+            <div className="mt-4">
+              <label htmlFor="eventDesc" className="block text-left text-m font-bold mb-1">Event Description: </label>
+              <textarea id="eventDesc" name="eventDesc" value={eventDesc} onChange={handleDescChange} required className="w-full h-full py-2 px-3 rounded border border-blue shadow align-top focus:border-black" />
+            </div>
+            <button type="submit" className="mt-5 bg-transparent hover:bg-gray-200 font-semibold py-2 px-4 border border-blue  rounded text-center">Submit form</button>
+          </form>
+
+          <div className="text-center bg-white shadow-md rounded-xl px-8 pt-6 pb-8 my-5 w-full h-max border border-blue">
+            <ExcelDisplay excelData={excelData} setExcelData={setExcelData} allEvents={allEvents} setAllEvents={setAllEvents}/>
           </div>
-          <div className="mt-4">
-            <label htmlFor="eventStart" className="block text-left text-m font-bold mb-1">Event Start:</label>
-            <input id="eventStart" type="datetime-local" name="eventStart" value={eventStart.toISOStringWithOffset().slice(0, 16)} onChange={handleStartChange} required
-              className="mx-auto shadow appearance-none border rounded w-full py-2 px-3 text-gray-500 leading-tight border-blue focus:border-black hover:cursor-pointer" />
-          </div>
-          <div className="mt-4">
-            <label htmlFor="eventEnd" className="block text-left text-m font-bold mb-1">Event End:</label>
-            <input id="eventEnd" type="datetime-local" name="eventEnd" value={eventEnd.toISOStringWithOffset().slice(0, 16)} onChange={handleEndChange} required
-              className="mx-auto shadow appearance-none border rounded w-full py-2 px-3 text-gray-500 leading-tight border-blue focus:border-black hover:cursor-pointer" />
-          </div>
-          <div className="mt-4">
-            <label htmlFor="selectedEventType" className="block text-left text-m font-bold mb-1">Event Type:</label>
-            <select id="selectedEventType" name="selectedEventType" value={eventType} onChange={handleTypeChange} required
-              className="mx-auto w-full bg-white border border-black px-3 py-2 rounded shadow leading-tight focus:border-black hover:cursor-pointer">
-              <Options categories={allEventTypes} />
-            </select>
-          </div>
-          <div className="mt-4">
-            <label htmlFor="eventDesc" className="block text-left text-m font-bold mb-1">Event Description: </label>
-            <textarea id="eventDesc" name="eventDesc" value={eventDesc} onChange={handleDescChange} required className="w-full h-full py-2 px-3 rounded border border-blue shadow align-top focus:border-black" />
-          </div>
-          <button type="submit" className="mt-5 bg-transparent hover:bg-gray-200 font-semibold py-2 px-4 border border-blue  rounded text-center">Submit form</button>
-        </form>
+        </section>
+
         <Calendar events={allEvents} handleEditClick={handleEditClick} handleSaveClick={handleSaveClick} selectedDay={selectedDay} setSelectedDay={setSelectedDay} eventsForSelectedDay={eventsForSelectedDay} setEventsForSelectedDay={setEventsForSelectedDay} handleDeleteClick={handleDeleteClick} />
       </main>
+    </div>
+  );
+}
+
+
+function ExcelDisplay({ excelData, setExcelData, allEvents, setAllEvents }) {
+
+  const handleFileUpload = (e) => {
+    const reader = new FileReader();
+    reader.readAsBinaryString(e.target.files[0]);
+    reader.onload = (e) => {
+      const data = e.target.result;
+      const workbook = XLSX.read(data, {type: "binary"});
+      const sheetName = workbook.SheetNames[0];
+      const sheet = workbook.Sheets[sheetName];
+      const parsedData = XLSX.utils.sheet_to_json(sheet);
+      setExcelData(parsedData);
+    }
+  }
+
+  function excelEvents() {
+    const updatedEvents = allEvents;
+    excelData.forEach((row) => {
+      let newEvent = {
+        name: row["Name"],
+        start: new CustomDate(row["Start Date"].replaceAll("/", "-")), // CustomDate object
+        end: new CustomDate(row["Start Date"].replaceAll("/", "-")), // CustomDate object
+        startDate: row["Start Date"],
+        endDate: row["End Date"],
+        startTime: row["Start Time"],
+        endTime: row["End Time"],
+        type: row["Type"],
+        description: row["Description"],
+        id: row["Name"] + uuidv4(),
+      }
+      newEvent.start.setHours(Number(newEvent.startTime.slice(0, 2)));
+      newEvent.start.setMinutes(Number(newEvent.startTime.slice(3, 5)));
+      newEvent.end.setHours(Number(newEvent.endTime.slice(0, 2)));
+      newEvent.end.setMinutes(Number(newEvent.endTime.slice(3, 5)));
+      console.log(row);
+      console.log(newEvent);
+      updatedEvents.push(newEvent);
+    });
+    console.log(updatedEvents);
+    setAllEvents([...allEvents, updatedEvents]);
+  }
+
+  return (
+    <div>
+      <label htmlFor="eventFile" className="block text-left text-m font-bold mb-1">Event File:</label>
+      <input id="eventFile" onChange={handleFileUpload} type="file" accept=".xlsx, .xls" name="eventStart" required
+        className="mx-auto shadow appearance-none border rounded w-full py-2 px-3 text-gray-500 leading-tight border-blue focus:border-black hover:cursor-pointer" />
+      <button onClick={excelEvents} className="my-5 bg-transparent hover:bg-gray-200 font-semibold py-2 px-4 border border-blue  rounded text-center">Load Excel Events</button>
+
+      {
+        excelData.length > 0 &&
+        <table>
+          <thead>
+            <tr>
+              {Object.keys(excelData[0]).map((key) => (
+                <th key={key}>{key}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {excelData.map((row, index) => (
+              <tr key={index}>
+                {Object.values(row).map((value, index) => (
+                  <td key={index}>{value}</td>
+                ))} 
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      }    
     </div>
   );
 }
@@ -501,7 +587,7 @@ function EventTable({ events, handleEditClick, handleSaveClick, handleDeleteClic
       <tbody>
         {
           (events.map(event => (
-            <Event key={event.id} eventObject={event} handleEditClick={handleEditClick} handleSaveClick={handleSaveClick} handleDeleteClick={handleDeleteClick} />
+            <Event key={event.id + uuidv4()} eventObject={event} handleEditClick={handleEditClick} handleSaveClick={handleSaveClick} handleDeleteClick={handleDeleteClick} />
           ))
           )
         }
