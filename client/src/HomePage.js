c61609b158739c3932b91b793702db62ee45d03a
Neola Dsouza <neola.laurend@gmail.com>
2024-01-25 18:54:01 -0500

    in the process of removing tailwind from Homepage

--- client/src/HomePage.js
+++ client/src/HomePage.js
@@ -6,39 +6,34 @@ import chapel from "./emmaus-pictures/chapel.jpg";
 import Footer from "./Footer.js";
 
 export default function HomePage() {
-  // structure:
-  // title with some background
-  // two text boxes with images (styled either stack or grid)
-  // upcoming events
-  // footer
   return (
     <div>
       <div className="title-container">
-        <div className="w-full p-5 mx-auto bg-white shadow-md font-semibold text-center text-xl italic">
-          <h4>Emmaus Wellness Centre is a place to come and meet the risen Jesus and cast away all fears and anxieties. </h4>
-          <h4>This is a unique and first of its kind centre in Goa.</h4>
+        <div className="title-banner">
+          <h1>Emmaus Wellness Centre is a place to come and meet the risen Jesus and cast away all fears and anxieties. </h1>
+          <h2>This is a unique and first of its kind centre in Goa.</h2>
         </div>
       </div>
 
       <div className="grid-container">
         <div className="grid-div">
-          <div className="text-grid">
-            <div className='bg-actualBlue-200 w-full mx-auto font-bold text-2xl text-center'>Who We Are</div>
+          <div className="text-section">
+            <div className='home-subheading'><h3>Who We Are</h3></div>
             <div>
               <p>Emmaus is a place seven miles away from Jerusalem. Two disciples of Jesus, after witnessing the brutal murder of their master, were walking towards Emmaus full of fear and anxiety. Risen Jesus meets them on their way.</p>
               <br/>
               <p>Since sorrow had overpowered the disciples, they could not recognize him. It is only at the breaking of the bread at Emmaus their eyes were opened and they recognized Him. Their fear and anxiety vanishes.</p>
             </div>
-            <Link to="/about"><button className="block bg-actualBlue-200 rounded-full py-2 px-4">About Us</button></Link>
+            <Link to="/about"><button className="linked-button">About Us</button></Link>
           </div>
-          <div className='shadow-md rounded'>
+          <div className='image shadow'>
             <img src={front} alt="" />
           </div>
-          <div className='bg-blue text-white shadow-md rounded'>
+          <div className='image shadow'>
             <img src={chapel} alt="" />
           </div>
-          <div className='text-grid'>
-            <div className='bg-actualBlue-200 w-full mx-auto font-bold text-2xl text-center'>What We Do</div>
+          <div className='text-section'>
+            <div className='home-subheading'>What We Do</div>
             <div>
               <p>Many programs, ranging from courses to retreats, are offered at Emmaus. We are open for week-long self-directed or guided retreats for religious as well as lay people throughout the year.
 We are also open for private workshops, seminars and meetings</p>
@@ -53,7 +48,7 @@ We are also open for private workshops, seminars and meetings</p>
                 <li><p>Weekday/Weekend Workshops</p></li>
               </ul>
             </div>
-            <Link to="/events"><button className="block bg-actualBlue-200 rounded-full py-2 px-4">Our Events and Programs</button></Link>
+            <Link to="/events"><button className="linked-button">Our Events and Programs</button></Link>
           </div>
         </div>
       </div>
